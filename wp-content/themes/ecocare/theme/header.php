74cdc0c474210c788d968aaa8f0bb86addf66251
Elisabeth Hamel <elisabethhamel@outlook.com>
2025-06-18 10:44:39 +0200

    favicon

--- wp-content/themes/ecocare/theme/header.php
+++ wp-content/themes/ecocare/theme/header.php
@@ -20,6 +20,13 @@
 	
 	<?php wp_head(); ?>
 
+	<link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+	<link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+	<link rel="shortcut icon" href="/favicon.ico" />
+	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+	<meta name="apple-mobile-web-app-title" content="Ecocare" />
+	<link rel="manifest" href="/site.webmanifest" />
+
 	<link rel="stylesheet" href="https://ecocare.ddev.site/wp-content/themes/ecocare/theme/aos.css" media="all">
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
